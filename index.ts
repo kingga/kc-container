588f7d28d50aef69f52834694439e13671697559
Isaac Skelton <isaacskelton16@gmail.com>
2019-11-23 20:20:34 +1300

    Fixed index.ts file.

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
-export * from './src/Class';
-export * from './src/Container';
-export * from './src/Service';
-export * from './src/ServiceType';
-export * from './src/contracts/IContainer';
+import Class from './src/Class';
+import Container from './src/Container';
+import Service from './src/Service';
+import ServiceType from './src/ServiceType';
+import IContainer from './src/contracts/IContainer';
+
+export default Container;
+export { Class, Service, ServiceType, IContainer };
